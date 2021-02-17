5b453869fdbf9b2c0f7371d8ca306bbe3504b413
garvitgupta13 <garvitgupta2001@gmail.com>
2021-02-17 22:52:42 +0530

    Added link in footer

--- get-pics/src/components/footer.jsx
+++ get-pics/src/components/footer.jsx
@@ -4,7 +4,15 @@ import "../App.css";
 export default function Footer() {
   return (
     <div className="footer">
-      Devloped by Garvit Gupta using
+      Devloped by{" "}
+      <a
+        href="https://www.linkedin.com/in/garvitgupta13/"
+        className="link"
+        target="blank"
+      >
+        Garvit Gupta
+      </a>{" "}
+      using{" "}
       <a href="https://pixabay.com/" className="link">
         Pixabay API
       </a>
