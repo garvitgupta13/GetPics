9f0c1a1b436b4e2e8a3926089c273de9581de33a
garvitgupta13 <garvitgupta2001@gmail.com>
2021-02-17 22:34:55 +0530

    Added footer component

--- get-pics/src/App.js
+++ get-pics/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import NavBar from "./components/navBar";
 import SearchBar from "./components/search";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <div className="container">
       <NavBar />
       <SearchBar />
+      <Footer />
     </div>
   );
 }
---
+++ get-pics/src/components/footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../App.css";
+
+export default function Footer() {
+  return (
+    <div className="footer">
+      Devloped by Garvit Gupta using
+      <a href="https://pixabay.com/" className="link">
+        Pixabay API
+      </a>
+    </div>
+  );
+}
